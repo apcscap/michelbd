b39d9beb984ca3609cbf86c17bbeee48a9bb5071
apcscap <42549554+apcscap@users.noreply.github.com>
2021-03-13 20:19:53 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+    $('label').click(function() {
+        $('label').removeClass('worngans');
+        $(this).addClass('worngans');
+    });
+});
+
+$(document).ready(function() {
+    $('#Start').click(function() {
+        $('#quiz2').show();
+    });
+});
+
+$(document).ready(function() {
+    $('#Fastlearning').click(function() {
+        $('#quiz3').show();
+    });
+});
+
+$(document).ready(function() {
+    $('#True').click(function() {
+        $('#quiz4').show();
+    });
+});
+
+$(document).ready(function() {
+    $('#Bool').click(function() {
+        $('#quiz5').show();
+    });
+});
+
+$(document).ready(function() {
+    $('#Bool').click(function() {
+        $('#quiz5').show();
+    });
+});
+
+$(document).ready(function() {
+    $('#Yes').click(function() {
+        $('#quiz6').show();
+    });
+});
+
+$(document).ready(function() {
+    $('#Rick').click(function() {
+        $('#quiz7').show();
+    });
+});
+
+
+
